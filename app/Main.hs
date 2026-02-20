20301fac197e776b61cab5503a23b06e25aba520
amkatelkin <amkatelkin@edu.hse.ru>
2026-02-20 20:34:29 +0300

    format evrything

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,13 @@
 import Control.Monad (forM_)
-
 import ParseInput
-import Types
 import Rules
+import Types
 
 main :: IO ()
 main = do
-    n <- readFile "test.txt"
-    let graph = parseInput n
-    let entry = filter (\v -> name v == "A") graph
-    forM_ rules $ \(rule, role) -> do
-        forM_ (rule entry) $ \v -> do
-            putStrLn $ show v ++ " " ++ role
+  n <- readFile "test.txt"
+  let graph = parseInput n
+  let entry = filter (\v -> name v == "A") graph
+  forM_ rules $ \(rule, role) -> do
+    forM_ (rule entry) $ \v -> do
+      putStrLn $ show v ++ " " ++ role
--- app/Types.hs
+++ app/Types.hs
@@ -2,20 +2,21 @@ module Types where
 
 data Gender = Male | Female | Any deriving (Eq, Show)
 
-data Vertex =
-    Vertex {
-        inc :: [Vertex],
-        out :: [Vertex],
-        dual :: [Vertex],
-        name :: String,
-        gender :: Gender
-    }
+data Vertex
+  = Vertex
+  { inc :: [Vertex],
+    out :: [Vertex],
+    dual :: [Vertex],
+    name :: String,
+    gender :: Gender
+  }
 
 instance Show Vertex where
-    show (Vertex inc_ out_ dual_ name_ gender_) =
-        unwords [
-            name_, show gender_,
-            show $ map name inc_,
-            show $ map name out_,
-            show $ map name dual_
-        ]
+  show (Vertex inc_ out_ dual_ name_ gender_) =
+    unwords
+      [ name_,
+        show gender_,
+        show $ map name inc_,
+        show $ map name out_,
+        show $ map name dual_
+      ]
