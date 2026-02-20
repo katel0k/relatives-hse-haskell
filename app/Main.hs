4b8fd32b435c822f7680c26081d77c6586e28a4b
amkatelkin <amkatelkin@edu.hse.ru>
2026-02-20 22:23:10 +0300

    Added argument parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,40 @@
 import Control.Monad (forM_)
+import Control.Monad.State (evalState)
+import qualified Data.HashSet as HS
+import Options.Applicative
 import ParseInput
 import Rules
 import Types
-import qualified Data.HashSet as HS
-import Control.Monad.State (evalState)
+
+data Options = Options
+  { optFilename :: FilePath,
+    optEntry :: String,
+    optDebug :: Bool
+  }
+
+options :: Parser Options
+options =
+  Options
+    <$> argument str (metavar "FILENAME" <> help "Input graph file")
+    <*> argument str (metavar "ENTRY" <> help "Entry name")
+    <*> switch (long "debug" <> help "Enable debug output")
 
 main :: IO ()
 main = do
-  n <- readFile "test.txt"
-  let graph = parseInput n
-  let entry = filter (\v -> name v == "A") graph
-  forM_ rules $ \(rule, role) -> do
-    let result = evalState (rule entry) HS.empty
-    forM_ result $ \v -> do
-      putStrLn $ show v ++ " " ++ role
+  opts <-
+    execParser $
+      info
+        (options <**> helper)
+        (fullDesc <> progDesc "Process graph with rules" <> header "mygraph")
+  let filename = optFilename opts
+      entryName = optEntry opts
+      debug = optDebug opts
+  do
+    content <- readFile filename
+    let graph = parseInput content
+        entry = filter (\v -> name v == entryName) graph
+        outputFunc = if debug then show else name
+    forM_ rules $ \(rule, role) -> do
+      let result = evalState (rule entry) HS.empty
+      forM_ result $ \v -> do
+        putStrLn $ outputFunc v ++ " " ++ role
